94795ba237186f5eaf23d5324452f85e7f84152b
María Simó <maria@coponstudio.es>
2019-11-26 12:01:21 +0100

    Added search near places axios request in main.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router  = express.Router();
+const Bar = require("../models/Bar");
+
 
 // Google apis
 let apiUrl;
@@ -11,12 +13,14 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-router.post('/location', (req, res, next) => {
-  res.json(req.body)
+router.get('/location', (req, res, next) => {
+ 
+  apiUrl=`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&key=${apiKey}`
+  res.json({apiUrl})
 });
 
 
-router.post('/results', (req, res, next) => {
+router.get('/results', (req, res, next) => {
   apiUrl=`https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
   res.render("results", {apiUrl});
 });
