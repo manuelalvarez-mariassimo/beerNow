4f12870d41461bb97c032bf827b24b7a901623e2
Manuel Álvarez Rosado <manuelalvarezrosado@MacBook-Air-de-Manuel.local>
2019-11-27 21:53:28 +0100

    Prueba htaccess

--- app.js
+++ app.js
@@ -36,6 +36,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
+app.use(function(req, res, next) {
+  var allowedOrigins = [ 'https://iron-beernow.herokuapp.com', 'http://iron-beernow.herokuapp.com'];
+  var origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
+
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
