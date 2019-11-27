74bf73f662ceada331caf76b8c8fdaa20054efce
Manuel Álvarez Rosado <manuelalvarezrosado@MacBook-Air-de-Manuel.local>
2019-11-27 16:25:07 +0100

    Routes and views beers and details

--- bin/seeds.js
+++ bin/seeds.js
@@ -16,7 +16,7 @@ const bcryptSalt = 10;
 
 
 mongoose
-  .connect(`mongodb://localhost/${process.env.DB}`, { useNewUrlParser: true })
+  .connect(`${process.env.DB}`, { useNewUrlParser: true })
   .then(x => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
--- routes/beer.js
+++ routes/beer.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router  = express.Router();
+const Beer = require("../models/Beer.js");
+const Comment = require("../models/Comments.js");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('beers/list');
-});
 
 // router.get('/:country', (req, res, next) => {
 //   res.render('beers/list');
@@ -14,9 +13,33 @@ router.get('/', (req, res, next) => {
 //   res.render('beers/details');
 // });
 
-router.get('/details', (req, res, next) => {
-  res.render('beers/details');
+router.get("/", (req, res) => {
+  Beer.find()
+    .then(beers => {
+      res.render('beers/list', { beers });
+    });
 });
+
+router.get("/details/:id", (req, res, next) => {
+  Beer.findById(req.params.id)
+    .then(beers =>
+      res.render('beers/details',  beers )
+    );
+});
+
+router.post("/details/:id", (req, res, next) => {
+  Comment.create({
+    comment: req.body.comment,
+    author: req.user._id
+  }).then((comment) => {
+    return Beer.findByIdAndUpdate( 
+      req.params.id, 
+      {$push: {comments: comment._id}},
+      {new: true})
+  })
+  .then(() => res.redirect("/"));
+});
+
 
 router.put('/:id/update', (req, res, next) => {
   res.render('beers/details');
