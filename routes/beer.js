8865341456cefc6cdb867e575a2e7783d1855acb
María Simó <maria@coponstudio.es>
2019-11-29 02:05:10 +0100

    Styled beer details view

--- routes/beer.js
+++ routes/beer.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 const Beer = require("../models/Beer.js");
 const Comment = require("../models/Comments.js");
 
@@ -14,31 +14,40 @@ const Comment = require("../models/Comments.js");
 // });
 
 router.get("/", (req, res) => {
-  Beer.find()
-    .then(beers => {
-      res.render('beers/list', { beers });
-    });
+  Beer.find().then(beers => {
+    res.render("beers/list", { beers });
+  });
 });
 
 router.get("/details/:id", (req, res, next) => {
   Beer.findById(req.params.id)
-    .then(beers =>
-      res.render('beers/details',  beers )
-    );
+    .populate([
+      {
+        path: "comments",
+        model: "Comment"
+      },
+      {
+        // todo
+        path: "author",
+        model: "User"
+      }
+    ])
+    .then(beers => res.render("beers/details", beers));
 });
 
 router.post("/details/:id", (req, res, next) => {
   Comment.create({
     comment: req.body.comment,
     author: req.user._id
-  }).then((comment) => {
-    return Beer.findByIdAndUpdate( 
-      req.params.id, 
-      {$push: {comments: comment._id}},
-      {new: true})
   })
-  .then(() => res.redirect("/"));
+    .then(comment => {
+      return Beer.findByIdAndUpdate(
+        req.params.id,
+        { $push: { comments: comment._id } },
+        { new: true }
+      );
+    })
+    .then(() => res.redirect("/"));
 });
-
 
 module.exports = router;
