e514639d50510428f31f6459f866858e615862e7
María Simó <maria@coponstudio.es>
2019-11-29 12:04:43 +0100

    Working at auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User");
 const Bar = require("../models/Bar");
-
 
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
@@ -11,38 +10,44 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
-
 router.get("/login", (req, res, next) => {
-  res.render("auth/login", {layout: false});
+  res.render("auth/login", { layout: false });
 });
-
-
 
 router.get("/profile/:id", (req, res, next) => {
-  Bar.find().then(b=>console.log(b))
-  User.findById(req.params.id)
-  .populate('favoriteBars')
-  .populate('favoriteBeers')
-   .then(user => {
-     console.log(user)
-      // res.json(user.favoriteBeers[0].name)
-      res.render('auth/profile', {user: user, layout: false} )
-   })
-   .catch(err => console.log(err))
+  
+  // if (req.session.currentUser) {
+  //   console.log(req.session.currentUser)
+
+    Bar.find().then(b => console.log(b));
+    User.findById(req.params.id)
+      .populate("favoriteBars")
+      .populate("favoriteBeers")
+      .then(user => {
+        console.log(user);
+        // res.json(user.favoriteBeers[0].name)
+        res.render("auth/profile", { user: user, layout: false });
+      })
+      .catch(err => console.log(err));
+  // } else {
+  //   res.redirect("/");
+  // }  
 });
 
-router.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/auth/login",
-  failureFlash: true,
-  passReqToCallback: true
-}));
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
 
 router.get("/signup", (req, res, next) => {
   res.render("auth/signup");
 });
-
-
 
 router.post("/signup", (req, res, next) => {
   const username = req.body.username;
@@ -50,8 +55,6 @@ router.post("/signup", (req, res, next) => {
   const country = req.body.country;
   const city = req.body.city;
   const password = req.body.password;
-
-
 
   if (username === "" || password === "") {
     res.render("auth/signup", { message: "Indicate username and password" });
@@ -67,22 +70,23 @@ router.post("/signup", (req, res, next) => {
     const salt = bcrypt.genSaltSync(bcryptSalt);
     const hashPass = bcrypt.hashSync(password, salt);
 
-      const newUser = new User({
-        username,
-        email,
-        country,
-        city,
-        password: hashPass
-      });
-  
-    newUser.save()
+    const newUser = new User({
+      username,
+      email,
+      country,
+      city,
+      password: hashPass
+    });
 
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(err => {
-      res.render("auth/signup", { message: "Something went wrong" });
-    })
+    newUser
+      .save()
+
+      .then(() => {
+        res.redirect("/");
+      })
+      .catch(err => {
+        res.render("auth/signup", { message: "Something went wrong" });
+      });
   });
 });
 
@@ -91,31 +95,31 @@ router.get("/logout", (req, res) => {
   res.redirect("/");
 });
 
-
 passport.use(
-  new GoogleStrategy({
+  new GoogleStrategy(
+    {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({
-          googleID: profile.id
-        })
+        googleID: profile.id
+      })
         .then(user => {
           if (user) {
             done(null, user);
             return;
           }
-          console.log(profile)
+          console.log(profile);
           const newUser = {
             username: profile.name.givenName,
             password: profile.id,
             email: profile.emails[0].value,
             country: null,
             city: null,
-            picture: profile.photos[0].value,
-          }
+            picture: profile.photos[0].value
+          };
           User.create(newUser)
             .then(newUser => {
               done(null, newUser);
@@ -127,7 +131,8 @@ passport.use(
   )
 );
 
-router.get("/google",
+router.get(
+  "/google",
   passport.authenticate("google", {
     scope: [
       "https://www.googleapis.com/auth/userinfo.profile",
@@ -142,6 +147,5 @@ router.get(
     failureRedirect: "/" // here you would redirect to the login page using traditional login approach
   })
 );
-
 
 module.exports = router;
